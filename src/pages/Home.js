193f4b6dfebe0fd2dd6771f39f03319a93a1f11e
noyes5 <noyes5@naver.com>
2023-09-21 16:09:19 +0900

    feat: 헤더 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <p>이곳은 메인 페이지 입니다.</p>
-        </div>
+
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
     );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
+  return (
+    <div>
+      <MyHeader 
+      headText={headText}
+      leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+      rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+    </div>
+  );
 };
 
 export default Home;
